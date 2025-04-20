b83c319c4f7789517ec28a440ea8fd15c78fb4f1
dawidtanski <dawtan96@gmail.com>
2025-04-20 14:17:55 +0200

    Still working on it

---
+++ inc/cub3D.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3D.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dtanski <dtanski@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/20 14:09:27 by dtanski           #+#    #+#             */
+/*   Updated: 2025/04/20 14:17:21 by dtanski          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB_3D_H
+
+# include "../libs/libft/libft.h"
+# include <mlx.h>
+# include <math.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+
+#endif
